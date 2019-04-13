84816697d263984be09794e7368dc0af433aba7e
PatCarey99 <49522191+PatCarey99@users.noreply.github.com>
2019-04-13 22:08:15 +1000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,71 +3,70 @@ import logo from './logo.jpg';
 import './App.scss';
 
 class App extends Component {
-  constructor () {
-    super()
-    this.state = {
-      num: 1
+    state = {
+        num: 1
     }
-    this.addNum = this.addNum.bind(this)
-    this.minusNum = this.minusNum.bind(this)
-  }
+    min = 1
+    max = 14
 
-  addNum() {
-    this.setState({
-      
-      num: this.state.num +1
-    }, function() {
-      console.log('number of issues is ' + this.state.num)
-    })
-  }
+    addNum = () => {
+        const { num } = this.state
+        if (num < this.max) {
+            this.setState({ num: num + 1 }, this.log)
+        }
+    }
 
-  minusNum() {
-    this.setState({
-      num: this.state.num - 1
-    }, function() {
-    })
-  }
+    minusNum = () => {
+        const { num } = this.state
 
-  render() { 
-     return (
-      <div>
+        if (num > this.min) {
+            this.setState({ num: num - 1 }, this.log)
+        }
+    }
+
+    log = () => {
+        console.log('number of issues is ' + this.state.num)
+    }
+
+    render() {
+        return (
+            <div>
+
         <img src={logo} alt="" className="logo"/>
-      <h2>Agenda</h2>
+            <h2>Agenda</h2>
 
-     <body><p className="paragraph">How many concerns would you like to discuss with your GP today?</p>
-      
-      <p> Although we might not be able to discuss ALL of them today, it's best for us to know them.
-       This will help provide more accurate health advice. </p>
-     
-      
-      <div className="incrementer">
-        <div>
-            <h2 className="minus" onClick={this.minusNum}>-</h2>
-        </div>
-        <div>
-        <h2 className="number">{this.state.num}</h2>
-        </div>
-        <div>
-           
-              <h2 className="plus" onClick={this.addNum}>+</h2>
+            <body><p className="paragraph">How many concerns would you like to discuss with your GP today?</p>
             
+            <p> Although we might not be able to discuss ALL of them today, it's best for us to know them.
+            This will help provide more accurate health advice. </p>
+            <div className="incrementer">
+                <div>
+                    <h2 className="minus" onClick={this.minusNum}>
+                        -
+                    </h2>
+                </div>
+                <div>
+                    <h2 className="number">{this.state.num}</h2>
+                </div>
+                <div>
+                    <h2 className="plus" onClick={this.addNum}>
+                        +
+                    </h2>
+                </div>
             </div>
-      </div>
-
-      </body>
-        <div className="btn"> 
-          <div className="btn-back">
-            Back
-          </div> 
-          <div className="btn-next" >
-            Next
-          </div>
-        </div>
-       
-      </div>
-        
-    );
-  }
+            </body>
+                <div className="btn"> 
+                <div className="btn-back">
+                    Back
+                </div> 
+                <div className="btn-next" >
+                    Next
+                </div>
+                </div>
+            </div>
+        );
+    }
 }
 
-export default App;
+export default App
+
